de0aa8cb195c20765002dd85eb714b430e05b52b
Trystan Hendricks <dolkhound@gmx.com>
2021-06-21 20:34:20 -0400

    function being messed up by public function

--- src/game.rs
+++ src/game.rs
@@ -31,6 +31,43 @@ impl Universe {
   fn get_index(&self, row: u32, column: u32) -> usize {
     (row * self.width + column) as usize
   }
+
+  pub fn tick(&mut self) {
+    let mut next = self.cells.clone();
+    for row in 0..self.height {
+      for col in 0..self.width {
+        let idx = self.get_index(row, col);
+        let cell = self.cells[idx];
+        let live_neighbours = self.live_neighbour_count(row, col);
+        next[idx] = match (cell, live_neighbours) {
+          (Cell::Alive, x) if x < 2 => Cell::Dead,
+          (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+          (Cell::Alive, x) if x > 3 => Cell::Dead,
+          (Cell::Dead, 3) => Cell::Alive,
+          (otherwise, _) => otherwise,
+        };
+      }
+    }
+    self.cells = next;
+  }
+
+  fn live_neighbour_count(&self, row: u32, column: u32) -> u8 {
+    let mut count = 0;
+    for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+      for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+        if delta_row == 0 && delta_col == 0 {
+          continue;
+        }
+
+        let neighbour_row = (row + delta_row) % self.height;
+        let neighbour_col = (column + delta_col) % self.width;
+        let idx = self.get_index(neighbour_row, neighbour_col);
+        count += self.cells[idx] as u8;
+      }
+    }
+    // calls the unsigned integer
+    count
+  }
 }
 
 // this creates the display of the blocks
